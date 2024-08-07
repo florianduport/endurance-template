381ccffaf482b36afa2ab684ff2570649092cf07
Florian Duport <f.duport@programisto.fr>
2024-08-07 10:20:48 +0200

    template with versioning example

--- modules/hello-world-local/routes/hello-world-local-v1.router.js
+++ modules/hello-world-local/routes/hello-world-local-v1.router.js
@@ -1,5 +1,14 @@
 const router = require('endurance-core/lib/router')();
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Hello World Local
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get("/",  (req, res) => {
   res.status(200).json({ message: 'Hello World Local!' });
 });
---
+++ modules/hello-world-local/routes/hello-world-local-v2.router.js
@@ -0,0 +1,17 @@
+const router = require('endurance-core/lib/router')();
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Hello World Local
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.get("/",  (req, res) => {
+  res.status(200).json({ message: 'Hello World Local! v2' });
+});
+
+
+module.exports = router;
